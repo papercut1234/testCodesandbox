5d308598010219c36945b7c78f68beb97835205f
papercut1234 <nao.public@gmail.com>
2021-04-25 17:30:26 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// const myprofile={
+//   name:"spiderman",
+//   age:28
+// };
+
+// const message1=`名前は${myprofile.name}で${myprofile.age}`;
+
+// console.log(message1);
+
+// const{name, age} = myprofile;
+
+// const message2=`${name},aaa${age}`;
+
+// console.log(message2);
+
+const myprofile = ["unko", 28];
+
+const [name, age] = myprofile;
+
+const message3 = `aaaa ${name} ${age}`;
+
+console.log(message3);
